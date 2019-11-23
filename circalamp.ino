76599453956d7f70566d55ca8fc4875a6f174d47
Roberto Santamaria Ayuso <roberto.santamaria@centurylink.com>
2019-11-23 17:44:06 +0100

    SoftwareSerial version

---
+++ circalamp.ino
@@ -0,0 +1,176 @@
+#include <SoftwareSerial.h>
+#include <Wire.h>
+#include <RTClib.h>
+
+
+#ifndef ESP01SPEED
+#define ESP01SPEED 9600
+#define SERIALSPEED 9600
+#define RX_PIN 2
+#define TX_PIN 7
+#endif
+
+
+const byte rxPin = RX_PIN;
+const byte txPin = TX_PIN;
+
+
+SoftwareSerial esp(rxPin, txPin); // RX, TX
+RTC_DS3231 rtc;
+
+
+int led = 13;
+String inputString = "";
+bool stringComplete = false;
+unsigned long lastEspChar;
+
+String alarmTime;
+bool alarmSwitch = false;
+
+
+void handleDemo(String command) {
+  if (command == "DEMO SET on") {
+    Serial.println("START DEMO");
+    digitalWrite(led, HIGH);
+  } else if (command == "DEMO SET off") {
+    Serial.println("STOP DEMO");
+    digitalWrite(led, LOW);
+  }
+
+}
+
+
+void handleTime(String command) {
+  delay(50);
+  if (command.startsWith("TIME GET")) {
+    DateTime now = rtc.now();
+    Serial.println("Sending " + now.timestamp());
+    esp.print(now.timestamp());
+    esp.write("\n");
+  } else if (command.startsWith("TIME SET")) {
+    String dateTime = command.substring(8);
+    dateTime.trim();
+    int i = 0;
+    int j = 0;
+    j = dateTime.indexOf('-');
+    String year_s = dateTime.substring(i, j);
+    int year_i = year_s.toInt();
+    i = j + 1;
+    j = dateTime.indexOf('-', i);
+    String month_s = dateTime.substring(i, j);
+    int month_i = month_s.toInt();
+    i = j + 1;
+    j = dateTime.indexOf('T');
+    String day_s = dateTime.substring(i, j);
+    int day_i = day_s.toInt();
+    i = j + 1;
+    j = dateTime.indexOf(':');
+    String hour_s = dateTime.substring(i, j);
+    int hour_i = hour_s.toInt();
+    i = j + 1;
+    j = i + 2;
+    String minute_s = dateTime.substring(i, j);
+    int minute_i = minute_s.toInt();
+    DateTime setTime = DateTime(year_i, month_i, day_i, hour_i, minute_i, 0);
+    Serial.println("Set Time " + setTime.timestamp());
+    rtc.adjust(setTime);
+  }
+
+}
+
+
+void handleAlarm(String command) {
+  if (command.startsWith("ALARM GET")) {
+    Serial.println(alarmTime);
+    esp.println(alarmTime);
+  }
+}
+
+void handleDebug(String command) {
+
+}
+
+void handleCommands(String command) {
+  Serial.println(">" + command + "<");
+  if (command.startsWith("DEMO")) {
+    handleDemo(command);
+  } else if (command.startsWith("DBG")) {
+    handleDebug(command);
+  } else if (command.startsWith("TIME")) {
+    handleTime(command);
+  } else if (command.startsWith("ALARM")) {
+    handleAlarm(command);
+  } else {
+    Serial.println("Invalid command");
+    esp.println("ERROR");
+  }
+}
+
+void setup()
+{
+  // set the data rate for the SoftwareSerial port
+  esp.begin(ESP01SPEED);
+
+  pinMode(led, OUTPUT);
+  // Open serial communications and wait for port to open:
+  Serial.begin(SERIALSPEED);
+  while (!Serial) {
+    ; // wait for serial port to connect. Needed for Native USB only
+  }
+
+
+  Serial.println("Finding RTC");
+  if (! rtc.begin()) {
+    Serial.println("Couldn't find RTC");
+  }
+  Serial.println("RTC Found");
+
+  if (rtc.lostPower()) {
+    Serial.println("RTC lost power, lets set the time!");
+    // following line sets the RTC to the date & time this sketch was compiled
+    rtc.adjust(DateTime(F(__DATE__), F(__TIME__)));
+    // This line sets the RTC with an explicit date & time, for example to set
+    // January 21, 2014 at 3am you would call:
+    // rtc.adjust(DateTime(2014, 1, 21, 3, 0, 0));
+  } else {
+    Serial.println("RTC time is OK");
+  }
+
+  Serial.println("Arduino start!");
+  char buf[] = "YYYYMMDDThh:mm:ss";
+  DateTime now = rtc.now();
+  Serial.println(now.toString(buf));
+  Serial.println(now.timestamp());
+  char alarmBuf[] = "hh:mm";
+  alarmTime = now.toString(alarmBuf);
+  lastEspChar = millis();
+}
+
+void loop() // run over and over
+{
+  if (esp.available()) {
+    char c = (char)esp.read();
+    inputString += c;
+    Serial.write(c);
+    if (c == '\n') {
+      stringComplete = true;
+    } else {
+      unsigned long now = millis();
+      if (now - lastEspChar > 2000) {
+        inputString = "";
+        inputString += c;
+        stringComplete = false;
+      }
+    }
+    lastEspChar = millis();
+  }
+  if (stringComplete) {
+    inputString.trim();
+    handleCommands(inputString);
+    stringComplete = false;
+    inputString = "";
+  }
+
+    if (Serial.available())
+      esp.write(Serial.read());
+}
